cac27ffec2c3ec09432972c6de0b0147c1183174
Saurabh Kumar Yadav <132043474+saurabhmlexplorer@users.noreply.github.com>
2024-04-09 23:02:54 +0530

    Create JavaBasics.java

---
+++ JavaBasics.java
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+                            JAVA 
+                            SAURABH KUMAR YADAV
+                            SHRADHA MAM
+                            Date: 09/04/2024
+
+*******************************************************************************/
+
+// BOILERPLATE CODE. 
+
+            /*  public class JavaBasics
+                {
+                	public static void main(String[] args) {
+                		
+                		
+                		
+                	}
+                }
+                */
+                
+        //Explanation: 1)In java you have to create a public class which have the same name as your file have.
+        //             2)Inside that class there should be a main function.
+        // NOTE: YOU WILL SEE THIS PEICE OF CODE IN ALMOST EVERY PROGRAM.
+
+// OUTPUT IN JAVA.
+//1. print -> it will print and do not provide next line. To get next line we can use \n (new line character).
+//2. println -> if we use println function it will automatically provide new line.
+
+
+
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.print("Welcome Saurabh!\n");
+        System.out.print("1 2 3 4 5 ");
+        
+        System.out.print("I am learning java.");
+        System.out.println("It will be a greate experience.");
+        System.out.println("I hope.");
+        System.out.print("Happy Coding...");
+	
+                		
+                		
+    }
+}
+
+
+
+
