fc36c00bd5156cf38078d7c4ed16eb3f1e7df42b
Saurabh Kumar Yadav <132043474+saurabhmlexplorer@users.noreply.github.com>
2024-04-12 20:23:20 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,19 @@
+
+public class variables
+{
+	public static void main(String[] args) {
+		int a = 32;
+		int b = 10;
+		
+		System.out.println("a");
+		System.out.println(a);
+		System.out.println(b);
+		
+		a = 39;
+		System.out.println(a);
+		
+		String name = "Saurabh Kumar Yadav";
+		System.out.println(name);
+		
+	}
+}
